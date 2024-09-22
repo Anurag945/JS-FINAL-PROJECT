feaca81d177804643a39d74c871afde47377efdd
anurag945 <anuragsinha945@gmail.com>
2024-09-22 16:40:04 +0530

    Page1 done

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ tl.to(".line h2",{
 tl.to("#loader",{
     opacity:0,
     duration:0.2,
-    delay:4
+    delay:0 // later change this to 4
 });
 tl.from("#page1",{
     delay:0.2,
@@ -40,6 +40,15 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none"
 });
+
+tl.from("#nav",{
+   opacity:0
+})
+
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2
+})
 
 }
 function mouscontrol(){
@@ -53,7 +62,7 @@ Shery.makeMagnet("#nav-part2 h4");
 }
 
 loadingAnimation();
-mouscontrol()
+mouscontrol();
 
 
 
